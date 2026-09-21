cb62a4fa3529844968ca089c16d13359191959db
Hassan Naeem <hassanbinnaeem1@gmail.com>
2026-09-21 12:33:07 +0500

    Api Created

---
+++ tests/api.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('GET /', () => {
+    test('should return Hello World', async () => {
+        const response = await request(app).get('/');
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({
+            message: 'Hello World'
+        });
+    });
+});
